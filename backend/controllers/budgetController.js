78e91ac406de092b87bf94765a9570b2f714fd28
danielsoleymani <daniel.soleymani12@gmail.com>
2025-06-19 10:29:06 -0400

    added async handlers

--- backend/controllers/budgetController.js
+++ backend/controllers/budgetController.js
@@ -1,27 +1,29 @@
-const getBudgets = (req, res)=>{
-    res.status(200).json({message : "Get all user budgets"})
-}
+const asyncHandler = require("express-async-handler")
 
-const createBudget = (req, res)=>{
+const getBudgets = asyncHandler(async (req, res)=>{
+    res.status(200).json({message : "Get all user budgets"})
+})
+
+const createBudget = asyncHandler(async (req, res)=>{
     const {name,email,phone} = req.body;
     if (!name || !email || !phone){
         res.status(400);
         throw new Error("All fields are required")
     }
     res.status(201).json({message : "Create budget"})
-}
+})
 
-const updateBudget = (req, res)=>{
+const updateBudget = asyncHandler(async (req, res)=>{
     res.status(200).json({message : `Update budget for ${request.params.id}`})
-}
+})
  
-const deleteBudget = (req, res)=>{
+const deleteBudget = asyncHandler( async (req, res)=>{
     res.status(200).json({message :`Delete budget for ${request.params.id}`})
-}
+})
 
-const getBudget = (req, res)=>{
+const getBudget = asyncHandler(async (req, res)=>{
     res.status(200).json({message :`Get budget for ${request.params.id}`})
-}
+})
 
 
 
